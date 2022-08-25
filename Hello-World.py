4697dd98c208a174f2f487dee08b1eda351dbf65
Bobbi Young <67978583+BobbiYoung@users.noreply.github.com>
2022-08-25 17:13:24 -0400

    Update Hello-World.py

--- Hello-World.py
+++ Hello-World.py
@@ -1,2 +1,9 @@
-#This program prints Hello, world! in Python
-print('Hello, world!')
+#This section prints Hello, world! in Python
+
+print("Hello, world!")
+
+#This section prints defined strings
+greeting = "Hello, world!"
+status = "just getting started here"
+who = "I'm"
+print(greeting, who, status)
